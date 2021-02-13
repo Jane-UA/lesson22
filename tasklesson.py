727f3fcb2d071137c2578312fb062021d9e656e3
Jane <73879019+Jane-UA@users.noreply.github.com>
2021-02-13 22:14:27 +0200

    tasks

--- tasklesson.py
+++ tasklesson.py
@@ -25,3 +25,18 @@ def first(self): # O(1)
     if self.__ppl:
         return self.__ppl.pop
     return None
+#пример Егорова Артема
+
+def power(x, n):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 1 / power(x, -n)
+    if n % 2 == 0:
+        return power(x, n // 2) * power(x, n // 2)
+    else:
+        return power(x, n - 1) * x
+
+x = int(input())
+n = int(input())
+print(power(x, n))
